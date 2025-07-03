7587e76141dc7fae1383a0450e7eaaedc8a5f5ec
Felipe Lopes <lopesffernandes@gmail.com>
2025-07-03 20:52:48 -0300

    add mlflow for model tracking

--- train_model.py
+++ train_model.py
@@ -2,10 +2,14 @@ import json
 import pathlib
 import pickle
 
+import mlflow
+from mlflow.models import infer_signature
 from sklearn import model_selection
 from sklearn import neighbors
 from sklearn import pipeline
 from sklearn import preprocessing
+from sklearn.metrics import mean_squared_error
+
 
 from create_model import SALES_PATH, DEMOGRAPHICS_PATH, SALES_COLUMN_SELECTION, OUTPUT_DIR, load_data
 
@@ -14,12 +18,49 @@ sales_demographic_data, target = load_data(SALES_PATH, DEMOGRAPHICS_PATH, SALES_
 
 
 x, y = load_data(SALES_PATH, DEMOGRAPHICS_PATH, SALES_COLUMN_SELECTION)
-x_train, _x_test, y_train, _y_test = model_selection.train_test_split(
+x_train, x_test, y_train, y_test = model_selection.train_test_split(
     x, y, random_state=42)
 
+
+params = {}
+
 model = pipeline.make_pipeline(preprocessing.RobustScaler(),
-                                neighbors.KNeighborsRegressor()).fit(
+                                neighbors.KNeighborsRegressor(**params)).fit(
                                     x_train, y_train)
+
+
+
+
+y_pred = model.predict(x_test)
+
+mse_test = mean_squared_error(y_test, y_pred)
+
+# Set our tracking server uri for logging
+mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
+
+# Create a new MLflow Experiment
+mlflow.set_experiment("MLflow Quickstart")
+
+# Start an MLflow run
+with mlflow.start_run():
+    # Log the hyperparameters
+    mlflow.log_params(params)
+
+    # Log the loss metric
+    mlflow.log_metric("MSE", mse_test)
+
+    # Infer the model signature
+    signature = infer_signature(x_train, model.predict(x_train))
+
+    # Log the model, which inherits the parameters and metric
+    model_info = mlflow.sklearn.log_model(
+        sk_model=model,
+        name="knn",
+        signature=signature,
+        input_example=x_train,
+        registered_model_name="tracking-quickstart",
+    )
+
 
 output_dir = pathlib.Path(OUTPUT_DIR)
 output_dir.mkdir(exist_ok=True)
